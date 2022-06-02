28f150cbd2bb13c3b861e823bc94dd357f55901c
Baudile KOUNNOU <baudilekounnou@MacBook-Pro-de-Baudile.local>
2022-06-02 23:29:19 +0100

    3-infinite_add.py add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
-    n = len(sys.argv)
-    sum = 0
-    for i in range(1, n):
-        sum += int(sys.argv[i])
-        print(sum)
+    import sys
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+        print("{}".format(result))
