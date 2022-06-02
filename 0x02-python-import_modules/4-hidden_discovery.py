136f5981ec5dc145142fe907d2aee3af7ea90f1c
Baudile KOUNNOU <baudilekounnou@MacBook-Pro-de-Baudile.local>
2022-06-02 23:41:42 +0100

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
 if __name__ == "__main__":
-    for name in dir(hidden_4):
-        if "__" not in name:
-            print(name)
+    from hidden_4 import *
+    arr = dir()
+    for i in range(0, len(arr)):
+        if arr[i][0:2] != "__":
+            print("{}".format(arr[i]))
